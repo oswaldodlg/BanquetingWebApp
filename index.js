9cf611094980f930faba4484633ef785084eb815
Oswaldodlg <oswaldodlg25@gmail.com>
2020-10-21 20:08:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
-app.get('/*', function(req,res) {
-
-    res.sendFile(path.join(__dirname+'/Client-App/public/index.html'));
-  });
 
 // SET UP MONGOOOSE 
 
