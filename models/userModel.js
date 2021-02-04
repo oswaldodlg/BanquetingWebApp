3abc85379c91d3f138b3e46a730261492da03a12
Oswaldodlg <oswaldodlg25@gmail.com>
2021-02-03 20:59:02 -0600

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required:true, unique: true, match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[banqueting.com]*$/ },
+    email: {type: String, required:true, unique: true},
     password: {type: String, required: true, minlength: 5},
     displayName: {type: String},
 });
 
 module.exports = User = mongoose.model("user", userSchema);
+
+
+//match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[banqueting.com]*$/ 
